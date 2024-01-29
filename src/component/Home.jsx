f17029bc28181752ffebcf1c28a75500734bfdc4
HetZer Soft <itzvakkoofficial@gmail.com>
2024-01-29 13:56:11 +0400

    First commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card text-bg-dark border-0">
+        <img
+          src="/assets/home-background.jpeg"
+          alt="bg"
+          height="750px"
+          className="card-img"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TREND</p>
+            <p className="card-text">
+              <small>Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "../App.css";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const state = useSelector((state) => state.handleCart);
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-white shadow-sm py-3">
+      <div className="container">
+        <NavLink className="navbar-brand fw-bold fs-4" to="/">
+          Collection-Be
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/products">
+                Product
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <div className="buttons">
+            <NavLink to="/login" className="btn btn-outline-dark ">
+              <i className="fa fa-sign-in me-1" />
+              Login
+            </NavLink>
+            <NavLink to="/register" className="btn btn-outline-dark ms-2">
+              <i className="fa fa-user-plus me-1" />
+              Register
+            </NavLink>
+            <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+              <i className="fa fa-cart-plus me-1" />
+              Cart ({state.length})
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
